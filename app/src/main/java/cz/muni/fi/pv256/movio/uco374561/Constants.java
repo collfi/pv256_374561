4d52cc043c9db1eb89f4995161a763b23bbf1ed8
Boris Valentovič <boris@oscarsenior.com>
2015-10-04 17:29:24 +0200

    api key

---
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco374561/Constants.java
@@ -0,0 +1,8 @@
+package cz.muni.fi.pv256.movio.uco374561;
+
+/**
+ * Created by collfi on 4. 10. 2015.
+ */
+public class Constants {
+    public static final String API_KEY = "c331638cd30b7ab8a4b73dedbbb62193";
+}
